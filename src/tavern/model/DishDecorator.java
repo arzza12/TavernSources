9f9cc7aa8fde93ec4d56e928c8d59d3b42b21606
arzza12 <nastya.rus1284@gmail.com>
2026-05-26 16:13:37 +0300

    Update DishDecorator.java

--- src/tavern/model/DishDecorator.java
+++ src/tavern/model/DishDecorator.java
@@ -1,14 +1,25 @@
 package tavern.model;
 
 public abstract class DishDecorator implements Dish {
+    protected abstract String getSaucename();
+    protected abstract int getSauceprice();
 
-    private Dish dish;
+    private final Dish dish;
+    
     public DishDecorator(Dish dish) {
         this.dish = dish;
     }
 
     public Dish getDish(){
         return dish;
+    }
+    @Override
+    public final String getName() {
+        return dish.getName() + getSaucename();
+    }
+     @Override
+    public final int getPrice() {
+        return dish.getPrice() + getSauceprice();
     }
 
 }
